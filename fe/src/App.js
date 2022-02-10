b0a687f7169469b38980e2f35faa65a3dce54046
Athiqur-r <athiqur-r@innomick.com>
2022-02-10 15:50:06 +0530

    first commit

---
+++ fe/src/App.js
@@ -0,0 +1,30 @@
+import logo from "./Raheman.jpg";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img
+          src={logo}
+          style={{ height: "350px", width: "270px" }}
+          alt="logo"
+        />
+        <p>
+          Helllo All, I am Raheman.
+          <br /> This webpage is created for testing purpose.
+        </p>
+        <a
+          className="App-link"
+          href="https://athiqur-r.github.io/cv/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Click here for my CV
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
